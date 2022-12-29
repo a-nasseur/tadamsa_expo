77517b4bbcdac1a2aa39b7d2a2f425fd6895a567
Abdelhak NASSEUR <a.nasseur.dev@gmail.com>
2022-12-29 16:45:58 +0100

    next seo

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,11 +1,50 @@
 import React from 'react'
 import Banner from '../src/components/Banner'
+import { NextSeo } from 'next-seo'
 
 type Props = {}
 
 const events = (props: Props) => {
   return (
+    <>
+      <NextSeo 
+         title="Tadamsa Expo | Évènements"
+         description="Organisateur d'événements sous la baniere des valeurs Tadamsa"
+         canonical="https://tadamsaexpo.com"
+         openGraph={{
+           url: 'https://www.tadamsaexpo.com',
+           title: 'Tadamsa Expo',
+           description: "Ready for tommorow's Algeria",
+           images: [
+             {
+               url: 'https://www.example.ie/og-image-01.jpg',
+               width: 800,
+               height: 600,
+               alt: 'Og Image Alt',
+               type: 'image/jpeg',
+             },
+             {
+               url: 'https://www.example.ie/og-image-02.jpg',
+               width: 900,
+               height: 800,
+               alt: 'Og Image Alt Second',
+               type: 'image/jpeg',
+             },
+             { url: 'https://www.example.ie/og-image-03.jpg' },
+             { url: 'https://www.example.ie/og-image-04.jpg' },
+           ],
+           siteName: 'SiteName',
+         }}
+         twitter={{
+           handle: '@handle',
+           site: '@site',
+           cardType: 'summary_large_image',
+         }}
+       
+    />
+
     <Banner title="Explorez nos évènements"  subtitle='Construisez avec nous'/>
+    </>
   )
 }
 
